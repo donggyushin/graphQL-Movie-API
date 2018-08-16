8062a3ab2b22d7e4a01cb65d2ecbd3e0d37c4892
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2018-08-16 14:03:08 +0900

    Done

--- graphql/db.js
+++ graphql/db.js
@@ -1,1 +1,34 @@
+import axios from "axios";
+
 const API_URL = "https://yts.am/api/v2/list_movies.json?limit=50";
+const API_DETAIL_URL = "https://yts.am/api/v2/movie_details.json";
+const API_SUGGESTIONS_URL = "https://yts.am/api/v2/movie_suggestions.json";
+
+export const getSuggestions = id => {
+  return axios
+    .get(API_SUGGESTIONS_URL, {
+      params: {
+        movie_id: id
+      }
+    })
+    .then(res => res.data)
+    .then(json => json.data.movies);
+};
+
+export const getMovies = () => {
+  return axios
+    .get(API_URL)
+    .then(res => res.data)
+    .then(json => json.data.movies);
+};
+
+export const getMovie = id => {
+  return axios
+    .get(API_DETAIL_URL, {
+      params: {
+        movie_id: id
+      }
+    })
+    .then(res => res.data)
+    .then(json => json.data.movie);
+};
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,6 +1,11 @@
+import { getMovies, getMovie, getSuggestions } from "./db";
+
 const resolvers = {
-  Query: {},
-  Mutation: {}
+  Query: {
+    movies: () => getMovies(),
+    movie: (_, { id }) => getMovie(id),
+    suggestions: (_, { id }) => getSuggestions(id)
+  }
 };
 
 export default resolvers;
